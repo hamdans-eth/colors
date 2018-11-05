ff5f52b265a51b1903588683d4daf31095327e44
hamdans <hamdans@student.ethz.ch>
2018-11-05 21:02:00 +0100

    rgb_to_hsv.

--- get_data.py
+++ get_data.py
@@ -3,7 +3,7 @@ pp=pprint.PrettyPrinter(indent='3')
 import numpy as np
 import sys
 sys.path.append('..')
-
+import numpy as np
 from data import munroecorpus
 
 
@@ -62,30 +62,63 @@ def make_pairs2(colors) :
     pairs = list(zip(colors,colors))
     return pairs, vocabulary
 
+def hsv_to_rgb(h,s,v) :
+    C = np.multiply(v, s)
+    X = C * (1 - (np.abs(np.mod(np.multiply(h, 1 / 60), 2) - 1)))
+    m = v - C
+    r_,g_,b_ = [],[],[]
+
+    for i, h in enumerate(h):
+
+        if h < 60:
+            r, g, b = C[i], X[i], 0
+        elif h < 120:
+            r, g, b = X[i], C[i], 0
+        elif h < 180:
+            r, g, b = 0, C[i], X[i]
+        elif h < 240:
+            r, g, b = 0, X[i], C[i]
+        elif h < 300:
+            r, g, b = X[i], 0, C[i]
+        elif h < 360:
+            r, g, b = C[i], 0, X[i]
+        r_.append(r + m[i])
+        g_.append(g + m[i])
+        b_.append(b + m[i])
+
+    return r_,g_,b_
+
 #training only
 def make_pairs3(colors, mode) :
     vocabulary = Vocabulary('eng')
     print("Read %s descriptions" % len(colors))
     RGB = []
-    if mode == 'training' :
+    if mode == 'train' :
         for i,description in enumerate(colors) :
             vocabulary.addDescription(description)
             #HSV values between 0 and 1
             h = munroecorpus.open_datafile(munroecorpus.get_training_filename(description, dim=0))
-            s = munroecorpus.open_datafile(munroecorpus.get_training_filename(description, dim=1))
-            v = munroecorpus.open_datafile(munroecorpus.get_training_filename(description, dim=2))
-            h = h * 360
-            C = v * s
-            X = C * (1 - (np.abs(h/60 % 2 -1)) )
-            m = v - C
-            switch 
+            s = np.multiply( munroecorpus.open_datafile(munroecorpus.get_training_filename(description, dim=1)) , 0.01)
+            v = np.multiply(munroecorpus.open_datafile(munroecorpus.get_training_filename(description, dim=2)),0.01)
+            RGB.append(hsv_to_rgb(h,s,v))
             colors[i] = description.replace('-',' ')
-    elif mode == 'test' : print('ok')
+    elif mode == 'test' :
+        #in hsv,
+        for i,description in enumerate(colors) :
+            vocabulary.addDescription(description)
+            #HSV values between 0 and 1
+            hsv = munroecorpus.open_datafile(munroecorpus.get_test_filename(description))
+            h = [ _[0] for _ in hsv]
+            s = np.multiply([ _[1] for _ in hsv],0.01)
+            v = np.multiply([ _[2] for _ in hsv],0.01)
+            RGB.append(hsv_to_rgb(h,s,v))
+            colors[i] = description.replace('-',' ')
     print("Counted words:")
     print(vocabulary.n_words)
     pairs = list(zip(colors,colors))
-    return pairs, vocabulary
-mode = 'training'
+    print(RGB)
+    return pairs, vocabulary, RGB
+mode = 'test'
 make_pairs3(make_list(),mode)
 
 
