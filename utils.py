f1ddb1f1607a29d7b87bca1a7964f7298df0f506
hamdans <hamdans@student.ethz.ch>
2018-12-03 21:46:30 +0100

    clean diag

--- utils.py
+++ utils.py
@@ -18,6 +18,23 @@ class CustomTrace(torch.autograd.Function):
         grad_input.mul_(grad_output[0])
         return grad_input
 
+def cholesky(A):
+    L = torch.zeros_like(A)
+
+    for i in range(A.shape[-1]):
+        for j in range(i+1):
+            s = 0.0
+            for k in range(j):
+                s = s + L[i,k].clone() * L[j,k].clone()
+
+            L[i,j] = torch.sqrt(A[i,i] - s) if (i == j) else \
+                      (1.0 / L[j,j].clone() * (A[i,j] - s))
+    return L
+
+
+
+
+
 def get_priors(RGB):
     #return clusters with mu and sigma for each RGB
     means = {}
@@ -27,54 +44,7 @@ def get_priors(RGB):
         G = [v[1] for v in RGB[key]]
         B = [v[2] for v in RGB[key]]
         means[key] = np.array([np.mean(R),np.mean(G), np.mean(B)])
-        variances[key] = np.cov( np.array([R, G, B]))
+        variances[key] = np.array([np.var(R), np.var(G), np.var(B)])
+        #variances[key] = np.cov( np.array([R, G, B]))
 
     return means,variances
-
-
-def sequence_mask(sequence_length, max_len=None):
-    if max_len is None:
-        max_len = sequence_length.data.max()
-    batch_size = sequence_length.size(0)
-    seq_range = torch.range(0, max_len - 1).long().to(device)
-    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
-    seq_range_expand = Variable(seq_range_expand).to(device)
-    if sequence_length.is_cuda:
-        seq_range_expand = seq_range_expand.cuda()
-    seq_length_expand = (sequence_length.unsqueeze(1)
-                         .expand_as(seq_range_expand))
-    return seq_range_expand < seq_length_expand
-
-
-def masked_cross_entropy(logits, target, length):
-    length = Variable(torch.LongTensor(length)).to(device)
-
-    """
-    Args:
-        logits: A Variable containing a FloatTensor of size
-            (batch, max_len, num_classes) which contains the
-            unnormalized probability for each class.
-        target: A Variable containing a LongTensor of size
-            (batch, max_len) which contains the index of the true
-            class for each corresponding step.
-        length: A Variable containing a LongTensor of size (batch,)
-            which contains the length of each data in a batch.
-    Returns:
-        loss: An average loss value masked by the length.
-    """
-
-    # logits_flat: (batch * max_len, num_classes)
-    logits_flat = logits.view(-1, logits.size(-1))
-    # log_probs_flat: (batch * max_len, num_classes)
-    log_probs_flat = functional.log_softmax(logits_flat)
-    # target_flat: (batch * max_len, 1)
-    target_flat = target.view(-1, 1)
-    # losses_flat: (batch * max_len, 1)
-    losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
-    # losses: (batch, max_len)
-    losses = losses_flat.view(*target.size())
-    # mask: (batch, max_len)
-    mask = sequence_mask(sequence_length=length, max_len=target.size(1))
-    losses = losses * mask.float()
-    loss = losses.sum() / length.float().sum()
-    return loss
