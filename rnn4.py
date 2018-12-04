covariance fixed autograd

rnn4.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 MAX_LENGTH = 4
 NUM_LAYERS = 1
+grad_clipping = 10
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class EncoderRNN(nn.Module):
@@ -23,10 +24,12 @@ class EncoderRNN(nn.Module):
     def forward(self, input, hidden):
         embedded = self.embedding(input).view(1, 1, -1)
         output, hidden = self.gru(embedded, hidden)
+        if hidden.requires_grad:
+            hidden.register_hook(lambda x: x.clamp(min=-grad_clipping, max=grad_clipping))
         output_m = torch.squeeze(self.embedding_to_RGB_m(output))
         f = torch.squeeze(self.embedding_to_RGB_v(output))
         #cov matrix
-        f = torch.Tensor([ [f[0], 0 , 0], [f[1],f[2] ,0], [f[3],f[4],f[5]] ])
+        #f = torch.Tensor([ [f[0], 0 , 0], [f[1],f[2] ,0], [f[3],f[4],f[5]] ])
         #Random lower triangular => covariance when multiplied f * f'
         return self.sigmoid(output_m).view(-1,1), f, hidden
 
@@ -54,48 +57,11 @@ class DecoderRNN(nn.Module):
             hidden = hidden.view([1,300])
             hidden = hidden.unsqueeze(0)
         output, hidden = self.gru(output, hidden)
+        if hidden.requires_grad:
+            hidden.register_hook(lambda x: x.clamp(min=-grad_clipping, max=grad_clipping))
         output = self.softmax(self.out(output[0]))
         #print(output)
         return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
-
-
-class AttnDecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
-        super(AttnDecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.dropout_p = dropout_p
-        self.max_length = max_length
-
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.attn = nn.Linear(self.hidden_size * 2, self.max_length+1)
-        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
-        self.dropout = nn.Dropout(self.dropout_p)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
-        self.out = nn.Linear(self.hidden_size, self.output_size)
-
-    def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
-        embedded = self.dropout(embedded)
-
-        #hidden = (torch.squeeze(hidden)).unsqueeze(0)
-
-        attn_weights = F.softmax(
-            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
-        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
-                                 encoder_outputs.unsqueeze(0))
-
-        output = torch.cat((embedded[0], attn_applied[0]), 1)
-        output = self.attn_combine(output).unsqueeze(0)
-
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-
-        output = F.log_softmax(self.out(output[0]), dim=1)
-        return output, hidden, attn_weights
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)

utils.py
@@ -7,7 +7,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class CustomTrace(torch.autograd.Function):
-
     def forward(self, input):
         self.isize = input.size()
         return input.new([torch.trace(input)])
@@ -34,7 +33,18 @@ def cholesky(A):
 
 
 
+def get_priors_(RGB):
+    #return clusters with mu and sigma for each RGB
+    means = {}
+    variances = {}
+    for key in RGB:
+        R = [v[0] for v in RGB[key]]
+        G = [v[1] for v in RGB[key]]
+        B = [v[2] for v in RGB[key]]
+        means[key] = np.array([np.mean(R),np.mean(G), np.mean(B)])
+        variances[key] = np.cov( np.array([R, G, B]))
 
+    return means,variances
 def get_priors(RGB):
     #return clusters with mu and sigma for each RGB
     means = {}

v4.py
@@ -10,7 +10,7 @@ import torchtext.vocab as vocab
 import numpy as np
 import time
 import math
-from utils import get_priors, CustomTrace
+from utils import get_priors_, CustomTrace,cholesky
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -19,19 +19,20 @@ SOS_token = 0
 EOS_token = 1
 PAD_token = 2
 MAX_LENGTH = 4
-epochs = 150000
+epochs = 15000
 rgb_dim_n = 3
 embedding_dim_n = 300
-learning_r = 0.1
+learning_r = 0.05
 alpha = 0.05 # for KL divergence
 trace = CustomTrace()
+grad_clipping = 10
 
 #Getting data (list of color descriptions)
 colors= make_list()
 #training pairs, vocabulary, dictionnary with a list of RGB values associated to every color
-pairs,vocabulary,RGB = make_pairs(colors,'test')
+pairs,vocabulary,RGB = make_pairs(colors,'train')
 
-means,log_variances=  get_priors(RGB)
+means,log_variances=  get_priors_(RGB)
 for k,v in log_variances.items() :
     s = np.all(np.linalg.eigvals(v) > 0)
     if not s : print('not')
@@ -97,23 +98,30 @@ def end_padding(input_tensor) :
     #print(input_tensor)
     return 0
 
-def cholesky(cov) :
-    L = torch.zeros_like(cov)
-    for i in range(cov.shape[-1]):
-        for j in range(i + 1):
-            s = 0.0
-            for k in range(j):
-                s = s + L[i, k] * L[j, k]
-            L[i, j] = torch.sqrt(cov[i, i] - s) if (i == j) else \
-                (1.0 / L[j, j] * (cov[i, j] - s))
-    return L
 
-def sample_z(mu, C):
+def mm_triangular(v,var) :
+    result = torch.zeros(3)
+    a = [ v[0].mul(var[0]) + v[1].mul(var[1]) + v[2].mul(var[3]),
+          v[1].mul(var[2]) + v[2].mul(var[4]),
+          v[2].mul(var[5]) ]
+    for i in range(3) :
+        result[i] = result[i] + a[i]
+    result = result.unsqueeze(0).t()
+
+    return result
+
+def sample_z(mu, var):
+    #var vector of length 6 such that
+    # a_0   0       0
+    # a_1   a_2     0
+    # a_3   a_4     a_5
     #reparam trick
-    C = torch.exp(C)
-    eps = torch.randn((3,1), requires_grad=True)
 
-    return (mu + torch.mm(C,eps)).squeeze()
+
+    eps = torch.randn((3,1), requires_grad=True)
+    result = (mu + mm_triangular(eps,var) ).squeeze()
+
+    return result
 
 def get_distance(input_tensor,current_RGB) :
     input_color_string = ''
@@ -132,17 +140,37 @@ def get_distance(input_tensor,current_RGB) :
     distance = mu * np.linalg.norm(np.subtract(current_RGB, target_rgb), 2)
     return mu * distance
 
+def det(a):
+    a = a.clone()
+    idx = [0,2,5]
+    det = torch.Tensor([1.])
+    for i in idx :
+        det = det * a[i]
+    return det.pow(2)
+
+def sq_triangular(a):
+    a = a.clone()
+    array =[[a[0].pow(2) , a[0].mul(a[1]) , a[0].mul(a[3])],
+                          [a[0].mul(a[1]) , a[2].pow(2) + a[1].pow(2), a[2].mul(a[4])],
+                          [a[0].mul(a[3]), a[2].mul(a[4]),a[3].pow(2) + a[4].pow(2) + a[5].pow(2)]]
+
+    squared = torch.zeros([3,3])
+    for i in range(3):
+        for j in range(3) : squared[i][j] = squared[i][j] + array[i][j]
+
+    return squared
+
+
+
 def kl_loss(mu_z,C_z,mu_t,cov_t) :
     #KL(p(z|x,y) || N(mu_y, cov_y))
-    #C_z = torch.exp(C_z)
+    cov_t = torch.tensor(cov_t,requires_grad=True).float()
+    cov_z =sq_triangular(C_z)
+    det_z = det(C_z)
     mu_t = torch.Tensor(mu_t).view(-1,1)
-    cov_z = torch.mm(C_z,C_z.t())
-    det_t = torch.Tensor([np.linalg.det(cov_t)])
+    det_t = torch.potrf(cov_t).diag().prod()
     cov_t = torch.Tensor(cov_t)
     cov_t_inv = cov_t.inverse()
-    det_z = (C_z.diagonal().prod())**2
-    # print(mu_z)
-    # print(cov_z)
     diff = mu_t - mu_z
     square = torch.mm(diff.t(),cov_t_inv)
     square = torch.mm(square,diff)
@@ -168,7 +196,7 @@ def train(input_tensor, target_tensor, encoder, decoder,linear, encoder_optimize
     #First we iterate through the words feeding tokens & last hidden state
     #print(input_length)
     for i in range(input_length):
-        mu_z,log_sigma_z, encoder_hidden = encoder(input_tensor[i], encoder_hidden)
+        mu_z,sigma_z, encoder_hidden = encoder(input_tensor[i], encoder_hidden)
 
 
     #Get the last RGB value
@@ -178,7 +206,9 @@ def train(input_tensor, target_tensor, encoder, decoder,linear, encoder_optimize
     log_sigma_t = log_variances[target]
     #print(mu_z,log_sigma_z)
     #print(mu_t,log_sigma_t)
-    current_RGB = sample_z(mu_z, log_sigma_z) # can be out of unit interval !!
+
+
+    current_RGB = sample_z(mu_z, sigma_z) # can be out of unit interval !!
 
 
     RGB_hidden = linear(current_RGB)
@@ -200,23 +230,22 @@ def train(input_tensor, target_tensor, encoder, decoder,linear, encoder_optimize
         loss += criterion(decoder_output, target_tensor[di])
         prediction = [prediction + [topi]]
         decoder_hidden = decoder_hidden + RGB_hidden # to get more info from RGB
-        #resample
-
-
 
 
     #LOSS
-    kl = kl_loss(mu_z,log_sigma_z,mu_t,log_sigma_t)
+    kl = kl_loss(mu_z,sigma_z,mu_t,log_sigma_t)
     rec = loss / (target_length - 1)
     loss = rec  + kl # minus sign ?
-    #print(loss.item())
     loss.backward()
+    torch.nn.utils.clip_grad_norm(encoder.parameters(), grad_clipping)
+    torch.nn.utils.clip_grad_norm(decoder.parameters(), grad_clipping)
+    torch.nn.utils.clip_grad_norm(linear.parameters(), grad_clipping)
 
     encoder_optimizer.step()
     decoder_optimizer.step()
     linear_optimizer.step()
     #print(loss.item()) #/ float(target_length) )
-    return loss.item(),kl.detach().numpy(),rec.detach().numpy() #/ float(target_length)  #+ distance
+    return loss.item() , kl.detach().numpy(),rec.detach().numpy() #/ float(target_length)  #+ distance
 
 
 ## utility functions
@@ -288,6 +317,7 @@ def trainIters(encoder, decoder,linear, n_iters, print_every=1000, plot_every=10
 
         loss,kl,rec = train(input_tensor, target_tensor, encoder,
                      decoder, linear, encoder_optimizer, decoder_optimizer, linear_optimizer, criterion)
+
         print_kl_total += kl
         print_rec_total += rec
 