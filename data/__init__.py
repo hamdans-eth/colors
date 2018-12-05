288ee528ce9a0d50fa738df8d2602de2f1de1b22
hamdans <hamdans@student.ethz.ch>
2018-12-05 17:27:29 +0100

    data files

---
+++ data/__init__.py
@@ -0,0 +1,3 @@
+#from __future__ import absolute_import
+#from . import munroecorpus.munroecorpus as munroecorpus
+from .munroecorpus import munroecorpus as munroecorpus
---
+++ data/rnn.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+def indexesFromDescription(lang, description):
+    return [lang.word2index[word] for word in description.split(' ')]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromDescription(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
